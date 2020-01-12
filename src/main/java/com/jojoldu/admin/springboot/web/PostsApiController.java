6e609e77df17ded55593e2455620d7b6044551ab
myeongseob91 <myeongseob91@gmail.com>
2020-01-12 22:28:12 +0900

    commit

---
+++ src/main/java/com/jojoldu/admin/springboot/web/PostsApiController.java
@@ -0,0 +1,24 @@
+package com.jojoldu.admin.springboot.web;
+
+import com.jojoldu.admin.springboot.service.posts.PostsService;
+import com.jojoldu.admin.springboot.web.dto.PostsSaveRequestDto;
+import com.jojoldu.admin.springboot.web.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+public class PostsApiController {
+
+    private final PostsService postsService;
+
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto) {
+        return postsService.save(requestDto);
+    }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
+        return postsService.update(id, requestDto);
+    }
+}
