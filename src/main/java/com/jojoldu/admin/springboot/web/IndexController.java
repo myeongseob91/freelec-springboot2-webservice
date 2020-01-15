70fd1c56288ab4e2776eb8a71c65446858f9944d
cppc <myeongseob91@naver.com>
2020-01-15 22:23:37 +0900

    google login complete

--- src/main/java/com/jojoldu/admin/springboot/web/IndexController.java
+++ src/main/java/com/jojoldu/admin/springboot/web/IndexController.java
@@ -1,4 +1,6 @@
 package com.jojoldu.admin.springboot.web;
+import com.jojoldu.admin.springboot.config.auth.LoginUser;
+import com.jojoldu.admin.springboot.config.auth.dto.SessionUser;
 import com.jojoldu.admin.springboot.service.posts.PostsService;
 import com.jojoldu.admin.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -13,8 +15,11 @@ public class IndexController {
 
     private final PostsService postsService;
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
+        if (user != null) {
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
